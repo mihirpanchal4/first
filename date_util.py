60ddbd420010c69aa4ec161396329154bb26b735
Mihir <mihirpanchal4@yahoo.com>
2018-01-13 16:40:18 +0530

    Deleted Hello World

---
+++ date_util.py
@@ -0,0 +1,9 @@
+# A function that will return only the current date
+
+# A function that will return  the current date
+
+# A function that will return only the current date
+
+## A function that will return only the current date
+
+ 
